1d819cca4cb9e3e92359cda91b81348672712201
tasticp_ <ricksue99@gmail.com>
2026-01-16 02:23:22 +0800

    powershell configs

---
+++ Powershell/ps1/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,33 @@
+# Suppress command echoing
+$ErrorActionPreference = 'SilentlyContinue'
+
+# UTF-8 encoding setup
+try {
+    [Console]::InputEncoding  = [System.Text.Encoding]::UTF8
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+    $OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+    chcp 65001 > $null 2>&1
+} catch {}
+
+# Oh My Posh initialization
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json" | Invoke-Expression
+
+Clear-Host
+
+# Fastfetch with random logo on startup
+if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
+    $logos = @(
+        "C:/Users/tasticp/.config/fastfetch/ascii.txt",
+        "C:/Users/tasticp/.config/fastfetch/ascii_cathey.txt"
+    )
+    fastfetch -c "C:/Users/tasticp/.config/fastfetch/config.jsonc" --logo (Get-Random -InputObject $logos)
+}
+
+# Function for manual runs
+function ff {
+    $logos = @(
+        "C:/Users/tasticp/.config/fastfetch/ascii.txt",
+        "C:/Users/tasticp/.config/fastfetch/ascii_cathey.txt"
+    )
+    fastfetch -c "C:/Users/tasticp/.config/fastfetch/config.jsonc" --logo (Get-Random -InputObject $logos)
+}
